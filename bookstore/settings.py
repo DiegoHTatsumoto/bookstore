6eeefc317d07452064827741952e9d47356231d7
Diego <diego.trabalhodev@gmail.com>
2026-01-19 21:20:42 -0300

    adicionando a paginação no projeto

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django_extensions",
     "product",
     "order",
+    "debug_toolbar",
 ]
 
 REST_FRAMEWORK = {
@@ -58,6 +59,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "bookstore.urls"
@@ -126,3 +128,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
